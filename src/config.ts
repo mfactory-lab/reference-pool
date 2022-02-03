fcc52a3508a484d6aa8e8c05c108d7c6f348e21f
tiamo <vk.tiamo@gmail.com>
2022-02-03 22:25:13 +0200

    chore: refactory

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,31 @@
+/*
+ * This file is part of Solana Reference Stake Pool code.
+ *
+ * Copyright © 2021, mFactory GmbH
+ *
+ * Solana Reference Stake Pool is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Solana Reference Stake Pool is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.
+ * If not, see <https://www.gnu.org/licenses/agpl-3.0.html>.
+ *
+ * You can be released from the requirements of the Affero GNU General Public License
+ * by purchasing a commercial license. The purchase of such a license is
+ * mandatory as soon as you develop commercial activities using the
+ * Solana Reference Stake Pool code without disclosing the source code of
+ * your own applications.
+ *
+ * The developer of this program can be contacted at <info@mfactory.ch>.
+ */
+
 import { Commitment, StakeProgram } from '@solana/web3.js';
 import { Endpoint } from '@/store';
 
@@ -5,16 +33,14 @@ const mode = import.meta.env.MODE;
 const isProd = mode == 'production';
 const isDev = mode == 'development';
 
-export const PASSWORD_PROTECT = '';
 export const DEFAULT_APY = 0.07;
 export const APY_VALIDATOR_ID = '8BYmtxKY1LuvjesaMi1nkXcj6ghuq48iiGKq2jNpnrNY';
-export const VALIDATORS_LIMIT = 60;
 export const EPOCH_RELOAD_DURATION = 60000;
 export const WITHDRAW_SOL_ACTIVE = true;
 export const SOL_USD_RELOAD_DURATION = 300000;
 export const POOL_CONNECTION_DELAY = 30000;
 
-export const GTAG_ID = isProd ? 'G-FXXRMT54JE' : null;
+export const GTAG_ID = isProd ? 'G-XXX' : null;
 
 // Stake
 export const STAKE_PROGRAM_ID = StakeProgram.programId;
